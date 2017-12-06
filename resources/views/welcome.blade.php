e6caa8fe01912ac151bbd55c295955741a9a8f0e
Dan Morton <dmorton2297@gmail.com>
2017-12-06 13:10:58 -0500

    Fixed button themeing

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -132,8 +132,8 @@
                                 <td id = "col">{!! $item->item !!}</td>
                                 <td id = "col">{!! $item->description !!}</td>
                                 <td id = "col">{!! $item->created_at !!}</td>
-                                <td id = "col"><button class="btn btn-default" onClick="<?php echo $function; ?>">Remove</button></td>
-                                <td id = "col"><button class="btn btn-danger" onClick="<?php echo $function2; ?>">Move</button></td>
+                                <td id = "col"><button class="btn btn-danger" onClick="<?php echo $function; ?>">Remove</button></td>
+                                <td id = "col"><button class="btn btn-default" onClick="<?php echo $function2; ?>">Move</button></td>
                              </tr>
                         @endif
                     @endforeach
